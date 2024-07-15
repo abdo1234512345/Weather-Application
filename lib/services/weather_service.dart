948eb6d85ca83d66f7e2e93e1bec9629bdc2d370
abdo1234512345 <41443491+abdo1234512345@users.noreply.github.com>
2024-07-15 19:02:22 +0200

    Weather App

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/models/weather_model.dart';
+
+class WeatherService {
+  String baseUrl = 'http://api.weatherapi.com/v1';
+
+  String apiKey = '0de419c0d0b5487ab82165100241507';
+  Future<WeatherModel> getWeather({required String cityName}) async {
+    Uri url =
+        Uri.parse('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=7');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 400) {
+      var data = jsonDecode(response.body);
+      throw Exception(data['error']['message']);
+    }
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    WeatherModel weather = WeatherModel.fromJson(data);
+
+    return weather;
+  }
+}
